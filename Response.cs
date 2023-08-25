71db5cdfda4a2a8233d9a47c0b4f8e3ff1062659
Aniruddha Mohite <44015127+animohite@users.noreply.github.com>
2023-08-25 21:15:09 +0530

    Add files via upload

---
+++ Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Userdetailsapp.Models
+{
+    public class Response
+    {
+        public int statuscode { get; set; }
+        public string statusMessage { get; set; }
+    }
+}
